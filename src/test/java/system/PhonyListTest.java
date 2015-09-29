3de4c1e817cb06611a88555adfec05cfd831e1ed
StephaneMangin <stephane.mangin@freesbee.fr>
2015-09-29 17:11:36 +0200

    Add tests skeleton

---
+++ src/test/java/system/PhonyListTest.java
@@ -0,0 +1,93 @@
+package system;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by stephane on 29/09/15.
+ */
+public class PhonyListTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+
+    }
+
+    @Test
+    public void testSize() throws Exception {
+
+    }
+
+    @Test
+    public void testIsEmpty() throws Exception {
+
+    }
+
+    @Test
+    public void testContains() throws Exception {
+
+    }
+
+    @Test
+    public void testIndexOf() throws Exception {
+
+    }
+
+    @Test
+    public void testElementData() throws Exception {
+
+    }
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testSet() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
+    @Test
+    public void testRemove() throws Exception {
+
+    }
+
+    @Test
+    public void testClear() throws Exception {
+
+    }
+
+    @Test
+    public void testAddAll() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveRange() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveAll() throws Exception {
+
+    }
+}
